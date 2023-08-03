57cec68f76db296bdcb9399f500a1a91de5185ef
wizkiye <wizkiye@gmail.com>
2023-08-03 15:10:33 +0300

    ¯\_(ツ)_/¯

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="telebirrTxChecker",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     url="https://github.com/wizkiye/TelebirrPaymentProcessor",
     license="MIT",
