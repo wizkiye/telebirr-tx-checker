1055962983e9dcf1cfb6afed04d47b5c4b2daf53
wizkiye <wizkiye@gmail.com>
2023-08-03 15:14:30 +0300

    ¯\_(ツ)_/¯

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
 setup(
@@ -9,6 +11,8 @@ setup(
     author="Kidus",
     author_email="wizkiye@gmail.com",
     description="asynchronous telebirr transaction checker",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "httpx>=0.24.1",
         "bs4>=0.0.1",
